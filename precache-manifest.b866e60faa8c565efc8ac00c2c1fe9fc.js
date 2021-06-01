5810fee0ae2fb3ee1153396277e9b13a9a279f5b
kirssy13 <kirssymoore@gmail.com>
2021-06-01 07:50:35 +0000

    Updates

---
+++ precache-manifest.b866e60faa8c565efc8ac00c2c1fe9fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493b710e6f2f9b9a691ea9a253230fb4",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d90102b9861a0fe2fe8c",
+    "url": "/home/static/css/main.7ed7da15.chunk.css"
+  },
+  {
+    "revision": "0d51b2267437f9020aee",
+    "url": "/home/static/js/2.ff56f5a3.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.ff56f5a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90102b9861a0fe2fe8c",
+    "url": "/home/static/js/main.b5b0e95b.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
